f13c5c724e102ff3a4b4ec281e0991974052e1d2
unknown <TAKhalopova@stud.kpfu.ru>
2025-07-16 20:01:14 +0300

    + base models

--- sport_analytics/api/models.py
+++ sport_analytics/api/models.py
@@ -1,7 +1,7 @@
 import json
 
 from django.db import models
-from django.contrib.postgres.fields import JSONField
+from django.db.models import JSONField
 from django.utils import timezone
 from django.core.exceptions import ValidationError
 
@@ -16,33 +16,91 @@ class BaseModel(models.Model):
         auto_now=True  # На обновление
     )
 
+    class Meta:
+        abstract = True
     def __str__(self):
         return (f'Время создания - {self.created_at}.'
                 f' Время обновления - {self.updated_at}')
 
 
 class Team(BaseModel):
-    pass
+    id = models.AutoField(
+        primary_key=True,
+        verbose_name='ID'
+    )
+    name = models.CharField(
+        'Название команды',
+        max_length=255
+    )
+    city_region = models.CharField(
+        'Город/Регион',
+        max_length=255
+    )
+
+    coaches = models.ManyToManyField(
+        'Coach',
+        related_name='teams',
+        verbose_name='Тренеры'
+    )
+
+    players = models.ManyToManyField(
+        'Player',
+        related_name='teams',
+        verbose_name='Игроки'
+    )
+
+    class Meta:
+        verbose_name = 'Команда'
+        verbose_name_plural = 'Команды'
+
+    def __str__(self):
+        return self.name
 
 
 class Coach(BaseModel):
-    pass
+    FIO_MAX_LENGTH = 255
+    CONTACT_MAX_LENGTH = 255
+
+    id = models.AutoField(
+        primary_key=True,
+        verbose_name='ID'
+    )
+
+    full_name = models.CharField(
+        'ФИО',
+        max_length=FIO_MAX_LENGTH
+    )
+    contact_data = models.CharField(
+        'Контактные данные',
+        max_length=CONTACT_MAX_LENGTH
+    )
+    birth_date = models.DateField(
+        'Дата рождения'
+    )
+    class Meta:
+        verbose_name = 'Тренер'
+        verbose_name_plural = 'Тренеры'
+
+    def __str__(self):
+        return self.full_name
 
 
 class Player(BaseModel):
     FIO_MAX_LENGTH = 255
+    MIN_EXPERIENCE = 0
+    MAX_EXPERIENCE = 100
 
     class Meta:
-        MIN_EXPERIENCE = 0
-        MAX_EXPERIENCE = 100
+        # MIN_EXPERIENCE = 0
+        # MAX_EXPERIENCE = 100
 
-        constraints = [
-            # Проверка диапазона стажа
-            models.CheckConstraint(
-                check=models.Q(experience__gte=MIN_EXPERIENCE) & models.Q(experience__lte=MAX_EXPERIENCE),
-                name='experience_range'
-            )
-        ]
+        # constraints = [
+        #     # Проверка диапазона стажа
+        #     models.CheckConstraint(
+        #         check=models.Q(experience__gte=models.F('MIN_EXPERIENCE')) & models.Q(experience__lte=models.F('MAX_EXPERIENCE')),
+        #         name='experience_range'
+        #     )
+        # ]
         verbose_name = 'Игрок'
         verbose_name_plural = 'Игроки'
 
@@ -57,17 +115,17 @@ class Player(BaseModel):
     birth_date = models.DateField('Дата рождения')
     experience = models.IntegerField(
         'Стаж',
-        max_length=Meta.MAX_EXPERIENCE,
-        default=Meta.MIN_EXPERIENCE
+        # max_length=MAX_EXPERIENCE,
+        default=MIN_EXPERIENCE
     )
-    team = models.ForeignKey(
-        Team,
-        on_delete=models.SET_NULL,
-        related_name='players',
-        null=True,
-        blank=True,
-        verbose_name='Команда'
-    )
+    # team = models.ForeignKey(
+    #     Team,
+    #     on_delete=models.SET_NULL,
+    #     related_name='players',
+    #     null=True,
+    #     blank=True,
+    #     verbose_name='Команда'
+    # )
     anthropometric = JSONField(
         'Антропометрические данные',
         null=True,
@@ -130,17 +188,17 @@ class Game(BaseModel):
     )
     datetime = models.DateTimeField('Дата и время проведения')
     teams = models.ManyToManyField(
-        Team,
+        'Team',
         related_name='games',
         verbose_name='Команды-участники'
     )
     players = models.ManyToManyField(
-        Player,
+        'Player',
         related_name='games',
         verbose_name='Игроки'
     )
     coaches = models.ManyToManyField(
-        Coach,
+        'Coach',
         related_name='games',
         verbose_name='Тренеры'
     )
@@ -178,4 +236,56 @@ class Game(BaseModel):
 
 
 class PlayerPosition(BaseModel):
-    pass
+    id = models.AutoField(
+        primary_key=True,
+        verbose_name='ID'
+    )
+    games = models.ForeignKey(
+        Game,
+        on_delete=models.CASCADE,
+        related_name='player_positions',
+        verbose_name='Игра'
+    )
+    players = models.ForeignKey(
+        Player,
+        on_delete=models.CASCADE,
+        related_name='player_positions',
+        verbose_name='Игрок'
+    )
+    timestamp = models.FloatField(
+        'Временная метка (секунда)',
+        help_text='В секундах от начала игры'
+    )
+    x_coordinate = models.FloatField(
+        'Координата X',
+        null=True,
+        blank=True
+    )
+    y_coordinate = models.FloatField(
+        'Координата Y',
+        null=True,
+        blank=True
+    )
+    z_coordinate = models.FloatField(
+        'Координата Z',
+        null=True,
+        blank=True
+    )
+    speed = models.FloatField(
+        'Скорость',
+        null=True,
+        blank=True
+    )
+    additional_parameters = JSONField(
+        'Дополнительные параметры',
+        null=True,
+        blank=True
+    )
+
+    class Meta:
+        verbose_name = 'Позиция игрока'
+        verbose_name_plural = 'Позиции игроков'
+
+    def __str__(self):
+        return f"Позиция игрока {self.player} в игре {self.game} на {self.timestamp} секунде"
+
--- sport_analytics/sport_analytics/settings.py
+++ sport_analytics/sport_analytics/settings.py
@@ -53,9 +53,13 @@ TEMPLATES = [
 WSGI_APPLICATION = 'sport_analytics.wsgi.application'
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": os.environ.get("POSTGRES_DB"),
+        "USER": os.environ.get("POSTGRES_USER"),
+        "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
+        "HOST": os.environ.get("POSTGRES_HOST"),
+        "PORT": int(os.environ.get("POSTGRES_PORT", 5432)),
     }
 }
 
